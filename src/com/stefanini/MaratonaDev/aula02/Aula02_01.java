e296bee6995a28d7807ccc85a49a05d993d6422b
Mariana Romeo <desenvolvedoramariromeo@gmail.com>
2022-07-25 10:16:00 -0300

    Aula 02 - Maratona Dev

---
+++ src/com/stefanini/MaratonaDev/aula02/Aula02_01.java
@@ -0,0 +1,8 @@
+package com.stefanini.MaratonaDev.aula02;
+
+public class Aula02_01 {
+    public static void main(String[] args) {
+        Aula02_02 aula = new Aula02_02();
+        System.out.println(aula);
+    }
+}
---
+++ src/com/stefanini/MaratonaDev/aula02/Aula02_02.java
@@ -0,0 +1,10 @@
+package com.stefanini.MaratonaDev.aula02;
+
+public class Aula02_02 {
+    public static void main(String[] args) {
+        int nota = 7;
+        String nome = "10";
+
+        System.out.println(nota);
+    }
+}
