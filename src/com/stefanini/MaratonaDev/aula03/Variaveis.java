754822115d7434c34d0518643b5b47087bade425
Mariana Romeo <desenvolvedoramariromeo@gmail.com>
2022-07-25 10:16:38 -0300

    Aula 03 - Maratona Dev

---
+++ src/com/stefanini/MaratonaDev/aula03/Variaveis.java
@@ -0,0 +1,29 @@
+package com.stefanini.MaratonaDev.aula03;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        byte numByte = -128; // 8 bits
+        short numShort = 12321; // 16 bits
+        int numInte = 2147483647; // 32 bits
+        long numLong = 214748364739l; //64 bits
+
+        float numFloat = 10.000f; // 32 bits
+        double numDouble = 100000000000.000; // 64 bits
+
+        char varChar = 'F'; // 16 bits
+
+        boolean varBoolean = true;  // 1 bit
+
+        String texto = "Bem vindos Devs!";
+        Integer numeroConta = 4000;
+        Long numeroLongo = 1213123L;
+
+//        Ele deve começar com uma letra minúscula;
+//        Nao deve comecar com caracteres especiaisç
+//        Se o nome for composto por mais de uma palavra, use CamelCase;
+//        Evite usar variaveis de uma caracter;
+
+        String nomeCompleto = "David Sousa";
+
+    }
+}
