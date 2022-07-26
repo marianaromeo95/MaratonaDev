16caf7fbffb8df535d7f569fe13993fa5ae954e0
Mariana Romeo <desenvolvedoramariromeo@gmail.com>
2022-07-26 16:20:51 -0300

    Aula 09 - Orientação a Objeto na prática 1

---
+++ src/com/stefanini/MaratonaDev/aula09/Carro.java
@@ -0,0 +1,19 @@
+package com.stefanini.MaratonaDev.aula09;
+
+public class Carro {
+
+    String marca;
+    String modelo;
+    int velocidadeMaxima;
+    int taxaAceleracao;
+    double quilometrosPorLitro;
+
+    void acelerar() {
+        System.out.println("Acelerando...");
+    }
+
+    void frear() {
+        System.out.println("Freando...");
+    }
+
+}
